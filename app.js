d20b5c8d53cadc01387463432c3fee87af76206c
chalaka-appuwahandi <88818216+chalaka-appuwahandi@users.noreply.github.com>
2021-11-19 15:48:45 +1100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    frontWindow = document.querySelector("#front-window-button"),
+    frontWindowRight = document.querySelector("#front-window-right-button"),
+    frontBumper = document.querySelector("#front-bumper-button")    ;
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -19,11 +22,30 @@ function cameraStart() {
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
+    alert("clicked");
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
+frontWindow.onclick = function() {
+    document.querySelector("#video_overlay_front_window").style.display = 'block';
+    document.querySelector("#video_overlay_front_window_right").style.display = 'none';
+    document.querySelector("#video_overlay_front_bumper").style.display = 'none';
+};
+
+frontWindowRight.onclick = function() {
+    document.querySelector("#video_overlay_front_window_right").style.display = 'block';
+    document.querySelector("#video_overlay_front_window").style.display = 'none';
+    document.querySelector("#video_overlay_front_bumper").style.display = 'none';
+};
+frontBumper.onclick = function() {
+    document.querySelector("#video_overlay_front_bumper").style.display = 'block';
+    document.querySelector("#video_overlay_front_window").style.display = 'none';
+    document.querySelector("#video_overlay_front_window_right").style.display = 'none';
+};
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
