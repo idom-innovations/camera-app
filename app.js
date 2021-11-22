ad2b16a766853d7a50374fd1b71c8fc500a2e6c2
chalaka-appuwahandi <88818216+chalaka-appuwahandi@users.noreply.github.com>
2021-11-22 11:39:41 +1100

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ function cameraStart() {
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
     });
+    document.querySelector("#camera").requestFullscreen();
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
